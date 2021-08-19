a5c942e6d1c0e02125255847230d3d4a57bf1726
Diego Roberto Antunes <drantunes@gmail.com>
2021-08-19 15:55:17 -0300

    Leitura de QRCode

--- lib/pages/qrcode_page.dart
+++ lib/pages/qrcode_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class QRCodePage extends StatefulWidget {
   QRCodePage({Key? key}) : super(key: key);
@@ -9,6 +10,31 @@ class QRCodePage extends StatefulWidget {
 
 class _QRCodePageState extends State<QRCodePage> {
   String ticket = '';
+  List<String> tickets = [];
+
+  readQRCode() async {
+    String code = await FlutterBarcodeScanner.scanBarcode(
+      "#FFFFFF",
+      "Cancelar",
+      false,
+      ScanMode.QR,
+    );
+    setState(() => ticket = code != '-1' ? code : 'Não validado');
+
+    // Stream<dynamic>? reader = FlutterBarcodeScanner.getBarcodeStreamReceiver(
+    //   "#FFFFFF",
+    //   "Cancelar",
+    //   false,
+    //   ScanMode.QR,
+    // );
+    // if (reader != null)
+    //   reader.listen((code) {
+    //     setState(() {
+    //       if (!tickets.contains(code.toString()) && code != '-1')
+    //         tickets.add(code.toString());
+    //     });
+    //   });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +54,7 @@ class _QRCodePageState extends State<QRCodePage> {
                 ),
               ),
             ElevatedButton.icon(
-              onPressed: () => {},
+              onPressed: readQRCode,
               icon: Icon(Icons.qr_code),
               label: Text('Validar'),
             ),
